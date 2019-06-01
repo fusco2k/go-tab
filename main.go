e122b34559952256573c72a998b4f3b97511be31
ÂFilipe Fusco <filipe.fusco@gmail.com>
2019-06-01 20:34:17 -0300

    using tab inside session

--- main.go
+++ main.go
@@ -1,21 +1,50 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
+
+	"github.com/fusco2k/go-tab/config"
+	"github.com/fusco2k/go-tab/sessions"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 func main() {
 	router := httprouter.New()
-	router.GET("/api/rest/:tab/new", newSession())
+	//dependency config
+	cl := config.NewSession("mongodb://localhost:27017")
+	defer cl.Disconnect(context.Background())
+	ss := &config.Session{CL: cl.Database("go-tab").Collection("session")}
+
+	router.GET("/api/rest/:tab/new", newSession(ss))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func newSession() httprouter.Handle {
+func newSession(ss *config.Session) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-
+		tab, err := strconv.Atoi(ps.ByName("tab"))
+		if err != nil {
+			log.Printf("Error converting the string: %s", err)
+		}
+		session := &sessions.Session{
+			// Tab: tabs.Tab{Number: int8(tab)},
+		}
+		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+		defer cancel()
+		err = ss.CL.FindOne(ctx, bson.M{"tab": bson.M{"number": tab}}).Decode(&session)
+		if err != nil && err.Error() == "mongo: no documents in result" {
+			log.Printf("Creating a new session")
+		} else {
+			log.Printf("Error decoding the new session cursor: %s", err)
+			cancel()
+		}
+		fmt.Println(session)
 	}
 }
--- sessions/models.go
+++ sessions/models.go
@@ -1,12 +1,14 @@
 package sessions
 
 import (
+	"github.com/fusco2k/go-tab/tabs"
 	"github.com/google/uuid"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
-type session struct {
-	id          primitive.ObjectID
-	secretToken uuid.UUID
-	time        primitive.DateTime
+type Session struct {
+	ID          primitive.ObjectID
+	SecretToken uuid.UUID
+	Tab         tabs.Tab
+	Time        primitive.DateTime
 }
