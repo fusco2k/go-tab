e31848ef99e419c2d69a47ae52f6cbf3bade8ac0
ÂFilipe Fusco <filipe.fusco@gmail.com>
2019-06-01 18:29:59 -0300

    drop gin to use pure httprouter

--- main.go
+++ main.go
@@ -9,6 +9,13 @@ import (
 
 func main() {
 	router := httprouter.New()
+	router.GET("/api/rest/:tab/new", newSession())
 
 	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func newSession() httprouter.Handle {
+	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	}
 }
