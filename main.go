eafd79256c39f424af89765b8f649d4e12fb8514
ÂFilipe Fusco <filipe.fusco@gmail.com>
2019-06-03 19:36:07 -0300

    basic session/redirect functions working

--- main.go
+++ main.go
@@ -23,54 +23,66 @@ func main() {
 	//dependency config
 	cl := config.NewSession("mongodb://localhost:27017")
 	defer cl.Disconnect(context.Background())
-	ss := &config.Session{CL: cl.Database("go-tab").Collection("session")}
+	sEnv := &config.Session{CL: cl.Database("go-tab").Collection("session")}
 	//template manager
 	tpl := config.TplManager()
 	//new httprouter
 	router := httprouter.New()
 	//routes
-	router.GET("/api/rest/open", openSS(tpl))
-	router.POST("/api/rest/new", newSS(ss))
+	router.GET("/api/rest/new", newSS(tpl))
+	router.POST("/api/rest/new", findSS(sEnv))
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
 
-func openSS(tpl *template.Template) httprouter.Handle {
+func newSS(tpl *template.Template) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-		err := tpl.ExecuteTemplate(w, "open.gohtml", nil)
+		err := tpl.ExecuteTemplate(w, "open.html", nil)
 		if err != nil {
 			log.Printf("Couldn't execute open.gohtml: %s", err)
 		}
 	}
 }
 
-func newSS(ss *config.Session) httprouter.Handle {
+func findSS(sEnv *config.Session) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
 		//parse the tab
-		tab, err := strconv.Atoi(ps.ByName("tab"))
+		tab, err := strconv.Atoi(r.FormValue("tab"))
 		if err != nil {
 			log.Printf("Error converting the string: %s\n", err)
 		}
-		session := &sessions.Session{}
+		ss := &sessions.Session{}
 		//check if the tab has an opened session, case not, create a new one
 		ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 		defer cancel()
-		err = ss.CL.FindOne(ctx, bson.M{"tab.number": 15}).Decode(&session)
+		err = sEnv.CL.FindOne(ctx, bson.M{"tab.number": tab}).Decode(&ss)
 		if err != nil && err.Error() == "mongo: no documents in result" {
-			log.Printf("Creating a new session")
-			fmt.Println(tab)
-			createSession(w, r, tab, session, ss)
-		}
-		if err != nil {
 			log.Printf("Error decoding the new session cursor: %s\n", err)
-			cancel()
+			log.Printf("Creating a new session")
+			createSS(w, r, tab, ss, sEnv)
+		} else {
+			log.Println("Checking the session")
+			checkSS(w, r, ss)
 		}
-		//print the session for debug
-		fmt.Println(session)
 	}
 }
 
-func createSession(w http.ResponseWriter, r *http.Request, tab int, session *sessions.Session, ss *config.Session) {
+func checkSS(w http.ResponseWriter, r *http.Request, ss *sessions.Session) {
+	cookie, err := r.Cookie("SessionID")
+	if err != nil {
+		log.Printf("Error getting cookie: %s\n", err)
+	}
+	switch cookie.Value == ss.SecretToken.String() {
+	case false:
+		fmt.Println("here")
+		//todo deal if the tab reports an opened session and the client already has a valid and opened session. Check why it has an opened session, that should not happen
+	case true:
+		r.Method = "GET"
+		http.Redirect(w, r, "/api/rest/"+strconv.Itoa(ss.Tab.Number), http.StatusFound)
+	}
+}
+
+func createSS(w http.ResponseWriter, r *http.Request, tab int, session *sessions.Session, ss *config.Session) {
 	//configure session
 	session.SecretToken = uuid.New()
 	session.Tab = tabs.Tab{
@@ -83,7 +95,6 @@ func createSession(w http.ResponseWriter, r *http.Request, tab int, session *ses
 	if cookie, err := r.Cookie("SessionID"); cookie != nil {
 		http.SetCookie(w, &http.Cookie{Name: "SessionID", MaxAge: -1})
 		log.Printf("Clearing cookie: %s\n", err)
-
 	} else if err != nil {
 		log.Printf("Empty cookie: %s\n", err)
 	}
