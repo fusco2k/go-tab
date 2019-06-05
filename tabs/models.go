4444233ca823c487b8a14f6976509393113076a1
ÂFilipe Fusco <filipe.fusco@gmail.com>
2019-06-05 01:24:25 -0300

    minor fix

--- tabs/models.go
+++ tabs/models.go
@@ -11,7 +11,8 @@ import (
 //Tab relational struct
 type Tab struct {
 	ID         primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
-	Number     int               `json:"number,omitempty" bson:"number,omitempty"`
+	Active     bool               `json:"active,omitempty" bson:"active,omitempty"`
+	Number     int                `json:"number,omitempty" bson:"number,omitempty"`
 	TimeOpened time.Time          `json:"timeopened,omitempty" bson:"timeopened,omitempty"`
 	Table      int8               `json:"table,omitempty" bson:"table,omitempty"`
 	Orders     []orders.Order     `json:"orders,omitempty" bson:"orders,omitempty"`
